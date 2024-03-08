00785daa8811becaf442b28287b0a0cf0ec32ccb
KpihX <kapoivha@gmail.com>
2024-03-08 09:24:36 +0100

    from props-types-exs to async-with-useffect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
@@ -9,18 +10,25 @@ import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error' 
 
+const GlobalStyle = createGlobalStyle`
+	div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-		<Route path="/survey/:questionNumber" element={<Survey />} />
-		<Route path="/results" element={<Results />}/>
-		<Route path="/freelances" element={<Freelances />}/>
-		<Route path="*" element={<Error />}/>
-	  </Routes>
-    </Router>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Router>
+			<GlobalStyle />
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/survey/:questionNumber" element={<Survey />} />
+				<Route path="/results" element={<Results />}/>
+				<Route path="/freelances" element={<Freelances />}/>
+				<Route path="*" element={<Error />}/>
+			</Routes>
+		</Router>
+	</React.StrictMode>,
   document.getElementById('root')
 )
